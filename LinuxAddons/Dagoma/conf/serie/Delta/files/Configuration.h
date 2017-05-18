8614683f57dba3a9b8b404947ce1f31d73a6e827
Christophe Andral <christophe@dagoma.fr>
2017-05-18 10:52:09 +0200

    Adjust MANUAL_Z_HOME_POS too tall

--- LinuxAddons/Dagoma/conf/serie/Delta/files/Configuration.h
+++ LinuxAddons/Dagoma/conf/serie/Delta/files/Configuration.h
@@ -732,7 +732,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the lo
   #define MANUAL_Y_HOME_POS 0
   //#define MANUAL_Z_HOME_POS 0
   //#define MANUAL_Z_HOME_POS 205 // For delta: Distance between nozzle and print surface after homing.
-  #define MANUAL_Z_HOME_POS 235 // 2017/03/10 For delta: Distance between nozzle and print surface after homing.
+  #define MANUAL_Z_HOME_POS 226 // 2017/03/10 For delta: Distance between nozzle and print surface after homing.
 #endif
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
