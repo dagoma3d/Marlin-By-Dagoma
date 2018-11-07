e758b9fdb222a33df25f64d2c3d1e17f84c13946
orel <orel@dagoma.fr>
2018-11-07 15:29:23 +0100

    Change STRING_CONFIG_H_AUTHOR.

--- LinuxAddons/Dagoma/conf/serie/Delta/files/Configuration.h
+++ LinuxAddons/Dagoma/conf/serie/Delta/files/Configuration.h
@@ -82,7 +82,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_CONFIG_H_AUTHOR "Dagoma.fr" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "Dagoma" // Who made the changes.
 #define SHOW_BOOTSCREEN
 #define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
 //#define STRING_SPLASH_LINE2 STRING_DISTRIBUTION_DATE // will be shown during bootup in line 2
