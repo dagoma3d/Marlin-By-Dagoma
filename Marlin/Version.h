5fa7541fd809c723ff9b432e30c29e718e9e4ae0
orel <orel@dagoma.fr>
2019-02-19 10:00:00 +0100

    Change distribution date to match the official announcement date...

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -42,7 +42,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "2018-12-18"
+#define STRING_DISTRIBUTION_DATE "2019-02-19"
 
 /**
  * @todo: Missing documentation block
